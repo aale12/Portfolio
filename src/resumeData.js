b3d547edd2ddfc07bbae42884284e4e88711616d
winkie224 <leanthony104@yahoo.com>
2019-02-23 17:02:24 -0600

    portfolio update

---
+++ src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Anthony Le",
+    "role": "Full Stack Web Develepor",
+    "linkedinId":"aale12",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a biology major turned developer.  I like dabbling in various parts of development and like to learn about new technologies, read technical articles, or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/aale12/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/aale12",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/aahle12",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I graduated from Texas A&M with a Bachelor's in biology. I decided to give development a try and fell in love. Now I am highly skilled in implementing core back-end development tasks including 3rd party API integration, website maintenance, REST API Development, etc. by deploying a wide range of technologies across multiple frameworks, software, Operating Systems & programming languages.  Adept at formulating & implementing strategies to execute key project deliverables & deliver compelling business value to clients.",
+    "address":"5007 E Newpark Dr, Houston Texas 77041",
+    "website":"https://aale12.github.io/Projects/Basic-Portfolio-Bootstrap/",
+    "education":[
+      {
+        "UniversityName":"Texas A&M University",
+        "specialization":"Biology, B.S.",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+        "Achievements":"a broad base for understanding principles governing life processes at all levels–molecular, cellular, organismal, and ecological."
+      },
+      {
+        "UniversityName":"The Houston Coding Boot Camp at UT Austin",
+        "specialization":"Certificate of Completion",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2019",
+        "Achievements":"A rigorous 24 week course, presented by the University of Texas Center for Professional Education, that develops the skills needed to build dynamic web applications and gain proficiency in the theory and application of web development"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"The Houston Coding Bootcamp at UT Austin",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2019",
+        "Achievements": [
+          "Designed and developed 20 web applications using technologies that include: JavaScript, CSS, Node.js, Express, Firebase, MySQL, MongoDB, and React.",
+          "Collaborated with other developers on group projects and established a project workflow using Github"
+        ]
+      },
+      {
+        "CompanyName":"River Oaks Nails & Spa",
+        "specialization":"Receptionist and Advertisement Manager",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"2019",
+        "Achievements": [
+          "Greeting and welcoming guests, provide them with a positive first impression of the business",
+          "Drove advertising initiatives which increased awareness of River Oaks Nail & Spa",
+          "Researched, identified and pursued opportunities to increase sales of both current and new customers"
+        ]
+      },
+      {
+        "CompanyName":"Quantum Physicians",
+        "specialization":"Medical Assistant Volunteer",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2016",
+        "Achievements": [
+          "Worked alongside medical assistants with various medical projects",
+          "Assisted with checking in patients, taking vital signs, and performing EKG"
+        ]
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
