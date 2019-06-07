a5a1fc502d427b5c8040e1f0d9f43b4f383ba187
winkie224 <leanthony104@yahoo.com>
2019-06-06 22:58:05 -0500

    updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,87 +1,127 @@
 import React, { Component } from 'react';
-export default  class Resume extends Component {
+export default class Resume extends Component {
+  constructor() {
+    super();
+    this.state = { open: false };
+  }
+  toggle() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
-         <div className="row education">
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            {
+              resumeData.education && resumeData.education.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.UniversityName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                      <p>
+                        {item.Achievements}
+                      </p>
                     </div>
-                  )
-                })
-              }
-            </div>
-         </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
         <div className="row work">
-            <div className="three columns header-col">
-               <h1><span>Work</span></h1>
-            </div>
-            <div className="nine columns main-col">
-              {
-                resumeData.work && resumeData.work.map((item) => {
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.CompanyName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
-                          <p>
-                          {item.Achievements.map((achievements) => {
-                            return(
-                              <div>
-                                <p>{achievements}</p>
-                              </div>
-                            )
-                          })}
-                          </p>
-                       </div>
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            {
+              resumeData.work && resumeData.work.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.CompanyName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                      <p>
+                        {item.Achievements.map((achievements) => {
+                          return (
+                            <div>
+                              <p>{achievements}</p>
+                            </div>
+                          )
+                        })}
+                      </p>
                     </div>
-                  )
-                })
-              }
-            </div> 
-         </div>
-         <div className="row skill">
-            <div className="three columns header-col">
-               <h1><span>Skills</span></h1>
-            </div>
-            <div className="nine columns main-col">
-               <p>
-               {resumeData.skillsDescription}
-               </p>
-   				<div className="bars">
-   				   <ul className="skills">
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            <p>
+              {resumeData.skillsDescription}
+            </p>
+            <div className="bars">
+              <ul className="skills">
                 {
                   resumeData.skills && resumeData.skills.map((item) => {
-                    return(
+                    return (
                       <li>
-                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
+                        <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                        </span><em>{item.skillname}</em>
                       </li>
                     )
                   })
                 }
-   					</ul>
-   				</div>
-   			</div>
-         </div>
+              </ul>
+            </div>
+          </div>
+          <div className="nine columns main-col">
+            <div className="allSkillsList">
+              <button className="btn btn-block" onClick={this.toggle.bind(this)}>
+                {this.state.open ? "Hide All Skills" : "Show All Skills"}
+              </button>
+              <div className={"collapse" + (this.state.open ? ' in' : '')}>
+                <div>
+                  {
+                    resumeData.allSkills.map((skill) => {
+                      return (
+                        <div>
+                          <ul className="allSkillsCategories">
+                            {skill.category}
+                          </ul>
+                          {
+                            skill.skills.map((item) => {
+                              return (
+                                <li>
+                                  {item}
+                                </li>
+                              )
+                            })
+                          }
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,129 +1,192 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Anthony Le",
-    "role": "Full Stack Web Develepor",
-    "linkedinId":"aale12",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I am a biology major turned developer.  I like dabbling in various parts of development and like to learn about new technologies, read technical articles, or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/aale12/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/aale12",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/aahle12",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I graduated from Texas A&M with a Bachelor's in biology. I decided to give development a try and fell in love. Now I am highly skilled in implementing core back-end development tasks including 3rd party API integration, website maintenance, REST API Development, etc. by deploying a wide range of technologies across multiple frameworks, software, Operating Systems & programming languages.  Adept at formulating & implementing strategies to execute key project deliverables & deliver compelling business value to clients.",
-    "address":"5007 E Newpark Dr, Houston Texas 77041",
-    "website":"https://aale12.github.io/Projects/Basic-Portfolio-Bootstrap/",
-    "education":[
-      {
-        "UniversityName":"Texas A&M University",
-        "specialization":"Biology, B.S.",
-        "MonthOfPassing":"May",
-        "YearOfPassing":"2016",
-        "Achievements":"a broad base for understanding principles governing life processes at all levels–molecular, cellular, organismal, and ecological."
-      },
-      {
-        "UniversityName":"The Houston Coding Boot Camp at UT Austin",
-        "specialization":"Certificate of Completion",
-        "MonthOfPassing":"Feb",
-        "YearOfPassing":"2019",
-        "Achievements":"A rigorous 24 week course, presented by the University of Texas Center for Professional Education, that develops the skills needed to build dynamic web applications and gain proficiency in the theory and application of web development"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"The Houston Coding Bootcamp at UT Austin",
-        "specialization":"Full Stack Web Developer",
-        "MonthOfLeaving":"Feb",
-        "YearOfLeaving":"2019",
-        "Achievements": [
-          "Designed and developed 20 web applications using technologies that include: JavaScript, CSS, Node.js, Express, Firebase, MySQL, MongoDB, and React.",
-          "Collaborated with other developers on group projects and established a project workflow using Github"
-        ]
-      },
-      {
-        "CompanyName":"River Oaks Nails & Spa",
-        "specialization":"Receptionist and Advertisement Manager",
-        "MonthOfLeaving":"Current",
-        "YearOfLeaving":"2019",
-        "Achievements": [
-          "Greeting and welcoming guests, provide them with a positive first impression of the business",
-          "Drove advertising initiatives which increased awareness of River Oaks Nail & Spa",
-          "Researched, identified and pursued opportunities to increase sales of both current and new customers"
-        ]
-      },
-      {
-        "CompanyName":"Quantum Physicians",
-        "specialization":"Medical Assistant Volunteer",
-        "MonthOfLeaving":"Jul",
-        "YearOfLeaving":"2016",
-        "Achievements": [
-          "Worked alongside medical assistants with various medical projects",
-          "Assisted with checking in patients, taking vital signs, and performing EKG"
-        ]
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      },
-      {
-        "skillname":"JavaScript"
-      },
-      {
-        "skillname":"Bootstrap 4.0"
-      },
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "images/bg.png",
+  "name": "Anthony Le",
+  "role": "Full Stack Web Develepor",
+  "linkedinId": "aale12",
+  "skypeid": "#",
+  "roleDescription": "I am a biology major turned developer.  I like dabbling in various parts of development and like to learn about new technologies, read technical articles, or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/aale12/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/aale12",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/aahle12",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "Full Stack Web Developer with background in Biology. Highly skilled in implementing core back-end development tasks including 3rd party API integration, website maintenance, REST API Development, etc. by deploying a wide range of technologies across multiple frameworks, software, Operating Systems & programming languages.  Excellent communication and effectively collaborates with others to ensure goals are achieved.  Resourceful problem-solver with superior conflict resolution skills.",
+  "address": "Houston, TX 77041",
+  "phone": "832-603-7739",
+  "email": "leanthony104@yahoo.com",
+  "education": [
+    {
+      "UniversityName": "The Houston Coding Boot Camp at UT Austin",
+      "specialization": "Certificate of Completion",
+      "MonthOfPassing": "Feb",
+      "YearOfPassing": "2019",
+      "Achievements": "A rigorous 24 week course presented by the University of Texas Center for Professional Education, that develops the skills needed to build dynamic web applications and gain proficiency in the theory and application of web development. Focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS."
+    },
+    {
+      "UniversityName": "Texas A&M University",
+      "specialization": "Biology, B.S.",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2016",
+      "Achievements": "Developed a broad base for understanding principles governing life processes at all levels–molecular, cellular, organismal, and ecological."
+    }
+
+  ],
+  "work": [
+    {
+      "CompanyName": "River Oaks Nails & Spa",
+      "specialization": "Receptionist and Advertisement Manager",
+      "MonthOfLeaving": "Current",
+      "YearOfLeaving": "2019",
+      "Achievements": [
+        "Greeting and welcoming guests, provide them with a positive first impression of the business.",
+        "Drove advertising initiatives which increased awareness of River Oaks Nail & Spa by use of graphic design.",
+        "Researched, identified and pursued opportunities to increase sales of both current and new customers by approximately 10%."
+      ]
+    },
+    {
+      "CompanyName": "Quantum Physicians",
+      "specialization": "Medical Assistant Volunteer",
+      "MonthOfLeaving": "Jul",
+      "YearOfLeaving": "2016",
+      "Achievements": [
+        "Worked alongside medical assistants with various medical projects such as handling correspondence and laboratory services.",
+        "Assisted with checking in patients, taking vital signs, and performing EKG"
+      ]
+    }
+  ],
+  "skillsDescription": "My Top Skills",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "mySQL"
+    },
+    {
+      "skillname": "MongoDB"
+    }
+  ],
+  "allSkills": [
+    {
+      "category": "Frameworks",
+      "skills": [
+        "Bootstrap",
+        "Materialize"
+      ]
+
+    },
+    {
+      "category": "Version Control System",
+      "skills": [
+        "Github"
+      ]
+    },
+    {
+      "category": "Databases",
+      "skills": [
+        "mySQL",
+        "MongoDB",
+        "Firebase"
+      ]
+    },
+    {
+      "category": "Libraries",
+      "skills": [
+        "React.js",
+        "d3.js",
+        "Express.js",
+        "Moment.js"
+      ]
+    },
+    {
+      "category": "Languages",
+      "skills": [
+        "JavaScript",
+        "SCSS/CSS",
+        "HTML5"
+      ]
+    },
+    {
+      "category": "Run-time Environment",
+      "skills": [
+        "Node.js"
+      ]
+    },
+    {
+      "category": "Cloud Platform",
+      "skills": [
+        "Git Pages",
+        "Heroku"
+      ]
+    },
+    {
+      "category": "ORM/ODM",
+      "skills": [
+        "Sequelize",
+        "Mongoose"
+      ]
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Tymbre",
+      "description": "Big data analyzer for music artists/recording companies, shows artist information and trends in a tiering system.  Stores, updates, and organizes artist information from Spotify, Last.FM, and iTunes into an easy to understand UI.",
+      "imgurl": "images/portfolio/origami.jpg",
+      "link": "https://tymbreapp.herokuapp.com/"
+    },
+    {
+      "name": "Hooligans",
+      "description": "RPG-Style, football themed fighting game.  Complete Character creation/customization and character background generation.  Choose your home city and face off against the A.I.",
+      "imgurl": "images/portfolio/console.jpg",
+      "link": "https://irichard03.github.io/Project1/index.html"
+    },
+    {
+      "name": "iPromise",
+      "description": "Timeline Manager, allows users to create, edit, and delete visual timelines.",
+      "imgurl": "/images/sample-image.jpg",
+      "link": "https://ipromise-2020.herokuapp.com/"
+    },
+    {
+      "name": "Rune Memory",
+      "description": "A memorization game.  Click all runes that you have not clicked before.  If you click one that you've already clicked, its Game Over!",
+      "imgurl": "/images/portfolio/coffee.jpg",
+      "link": "https://aale12.github.io/Rune-Memory/"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
